3516f2b15d55308a273f9f8ae71e75431c2b4916
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-02-12 15:24:16 +0100

    Visualizzare a schermo tutti i film dal DB sottoforma di card

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,19 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+    <title>laravel-model-controller</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+    @foreach ($movies as $movie)
+        <div class="card m-1" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Title: {{ $movie['title'] }}</h5>
+                <h5 class="card-title">Original title: {{ $movie['original_title'] }}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Nationality: {{ $movie['nationality'] }}</li>
+                <li class="list-group-item">Date: {{ $movie['date'] }}</li>
+                <li class="list-group-item">Vote:{{ $movie['vote'] }}</li>
+            </ul>
+        </div>
+    @endforeach
 @endsection
